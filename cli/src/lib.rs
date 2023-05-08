39e8d5942c5f39fa941b222f96acc2a42fbf20c7
Shamanth Kuthpadi <shamanth.kuthpadi2003@gmail.com>
2023-05-08 00:36:21 -0400

    Refactored and included functionalities.

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,8 +1,47 @@
-pub fn extract_args(args: &[String]) -> (&str, &str, &str, &str){
-    let io = &args[1];
-    let query = &args[2];
-    let filename = &args[3];
-    let msg = &args[4];
+use std::fs; // used for file system functionalities
+use std::io;
+use std::io::Write;
+use std::path::Path;
 
-    (io, query, filename, msg)
+pub fn create_file(args: &[String]) -> (){
+    let query = &args[1];
+    let filename = &args[2];
+
+    fs::File::create(filename)
+            .expect("something went wrong with creating the file ");
+
+    println!("File has been created with the name: {}", filename);   
+}
+
+pub fn read_file(args: &[String]) -> String{
+    let filename = &args[2];
+    let contents = fs::read_to_string(filename)
+        .expect("something went wrong with opening the file");
+    contents
+}
+
+pub fn write_to_file(args: &[String]) -> (){
+    let filename: &String = &args[2];
+    let mut rs:bool=true;
+    rs = Path::new(filename).exists();
+
+    println!("Please include the message you would like to write to {}", filename);
+    let mut msg = String::new();
+    io::stdin().read_line(&mut msg).unwrap();
+
+    if rs == true{
+        let mut file = fs::OpenOptions::new()
+        .append(true)
+        .open(filename)
+        .expect("cannot open file");
+        
+        file.write(msg.as_bytes()).expect("failed to write to file");
+    }
+    else {
+        let mut file = fs::File::create(filename)
+            .expect("something went wrong with opening the file for writing");
+
+        file.write(msg.as_bytes()).expect("writing failed");
+    }
+    println!("message appended to file");
 }
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,34 +1,21 @@
 mod lib;
 
 use std::env; // used to extract the arguments from cli
-use std::fs; // used for file system functionalities
-use std::io::Write; // used for writing to the file
 
 fn main() {
+    // collects the arguments as a vector of strings.
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 5{
-        panic!("Not enough arguments. Usage: <operation: R, W, C> <query> <filename> <message>");
-    }
+    let query = &args[1];
 
-    let (io, query, filename, msg) = lib::extract_args(&args);
-    println!("({}, {}, {}, {})", io, query, filename, msg);
-
-    let contents;
-    if io == "R"{
-        contents = fs::read_to_string(filename)
-            .expect("something went wrong with opening the file");
+    if query == "R"{
+        let contents = lib::read_file(&args);
         println!("{}", contents);
     }
-    if io == "C"{
-        fs::File::create(filename)
-            .expect("something went wrong with creating the file ");
-        println!("File has been created with the name: {}", filename);
+    if query == "C"{
+        lib::create_file(&args);
     }
-    if io == "W" {
-        let mut file = fs::File::create(filename)
-            .expect("something went wrong with opening the file for writing");
-        file.write(msg.as_bytes()).expect("writing failed");
-        println!("--{}-- has been written to file with name {}", msg, filename);
+    if query == "W" {
+        lib::write_to_file(&args);
     }
 }
